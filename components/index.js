0ac92f931680bf322e62c459f8ed24903b6ae07d
Mayank <mayankhp@outlook.com>
2021-04-17 17:55:39 +0530

    UPDATE: Create Theme Context

--- components/index.js
+++ components/index.js
@@ -1,7 +1,10 @@
+import React from "react";
+const ThemeContext = React.createContext();
+
 import Hero from "@components/LandingPage/Hero";
 import Features from "@components/LandingPage/Features";
 import Work from "@components/LandingPage/Work";
 import Banner from "@components/LandingPage/Banner";
 import Footer from "@components/LandingPage/Footer";
 
-export { Hero, Features, Work, Banner, Footer };
+export { ThemeContext, Hero, Features, Work, Banner, Footer };
