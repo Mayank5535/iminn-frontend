6ce524da14a0b531c86ee46871a8c6be961e663f
Mayank <mayankhp@outlook.com>
2021-04-19 23:23:30 +0530

    FIX: Design Issue

--- components/LandingPage/Work/index.js
+++ components/LandingPage/Work/index.js
@@ -1,20 +1,29 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useRef } from "react";
 import Images from "@config/images";
 import { Col, Row } from "antd";
+import useMediaQuery from "utils/useMediaQuery";
 
 function Banner() {
+  const { isXs, isSm } = useMediaQuery;
+  const listRef = useRef();
+
   return (
     <section className="commonContainer">
       <Row className="sectionHeading semiBold" justify="center" align="middle">
         <span>HOW IT&nbsp;</span>
         <span className="primaryColor">WORK</span>
       </Row>
-      <Row justify="space-between" align="top" className="workBody">
+      <Row
+        justify="space-between"
+        align="top"
+        className="workBody"
+        style={{ minHeight: listRef?.current?.clientHeight || "100%" }}
+      >
         <img src={Images.vectorPhone1} className="vectorsImg1" />
         <img src={Images.vectorPhone2} className="vectorsImg2" />
         <span className="backCircle" />
-        <Row className="workListContainer">
+        <Row className="workListContainer" ref={listRef}>
           <Col xs={{ span: 24 }} lg={{ span: 6 }} className="left">
             <div className="cirlceNumber  primaryLightBg colFlex allCenter primaryColor bold">
               1
