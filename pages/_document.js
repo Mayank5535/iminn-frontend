6daac63f8405001f46a407de1bd81352835df1fc
Mayank <mayankhp@outlook.com>
2021-04-30 22:01:42 +0530

    FIX: Fonts Updated

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,9 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap"
             rel="stylesheet"
+            crossOrigin="true"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap"
